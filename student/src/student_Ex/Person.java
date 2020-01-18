ecbb618a83030493ba3590c27dbc8a0845f4e0d9
pjt-tech <dktkd7044@gmail.com>
2020-01-18 22:10:51 +0900

    student

---
+++ student/src/student_Ex/Person.java
@@ -0,0 +1,15 @@
+package student_Ex;
+
+public class Person {
+	
+	String name;
+	int heignt;
+	double weight;
+	char gender;
+	boolean married;
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
---
+++ student/src/student_Ex/Stundent.java
@@ -0,0 +1,21 @@
+package student_Ex;
+
+public class Stundent {
+	
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	
+public String getshowStudentInfo() {
+		
+		return studentName;
+	}
+
+public void setshowStudentInfo(String name) {
+	
+	this.studentName = name;
+}
+
+}
