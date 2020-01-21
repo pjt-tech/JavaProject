fef34ef1ce74b03135671f75537cf762881a6aaa
pjt-tech <dktkd7044@gmail.com>
2020-01-21 21:47:57 +0900

    java[tire]

--- chap14/src/method_overradding/Car_Example.java
+++ chap14/src/method_overradding/Car_Example.java
@@ -13,19 +13,18 @@ public class Car_Example {
 				break;
 				
 				case 2 :
-					System.out.println("앞왼쪽 Hankook Tire로 교체");
+					System.out.println("앞오른쪽 kumho Tire로 교체");
 					car.frontRightTire = new kumhoTire(13,"앞오른쪽");
 					break;
 				case 3 :
 					System.out.println("뒤왼쪽 Hankook Tire로 교체");
-					car.backLeftTire = new HankookTire(17,"앞왼쪽");
+					car.backLeftTire = new HankookTire(17,"뒤왼쪽");
 					break;
 				case 4 :
-					System.out.println("뒤오른쪽 Hankook Tire로 교체");
-					car.backRightTire = new HankookTire(14,"앞왼쪽");
+					System.out.println("뒤오른쪽 kumho Tire로 교체");
+					car.backRightTire = new kumhoTire(14,"뒤오른쪽");
 					break;
 			}
-			
 			System.out.println("------------------------------");
 		}
 		
--- chap14/src/method_overradding/HankookTire.java
+++ chap14/src/method_overradding/HankookTire.java
@@ -21,6 +21,4 @@ public class HankookTire extends Tire {
 	
 	}
 	
-	
-
 }
