0a15a9ccbffee73515a2e24d474bb72d1222d986
pjt-tech <dktkd7044@gmail.com>
2020-01-29 22:03:26 +0900

    java[a]

---
+++ exercise/src/exercise_object05/EX03.java
@@ -0,0 +1,26 @@
+package exercise_object05;
+
+class A{
+	
+	public String x( ) {return "A.x";}
+	
+}
+
+class B extends A{
+	public String x() {return "B.x";}
+	public String y() {return "y";}
+}
+class B2 extends A{
+	public String x() {return "B2.x";}
+}
+public class EX03 {
+
+	public static void main(String[] args) {
+		A obj = new B();
+		A obj2 = new B2();
+		System.out.println(obj.x());
+		System.out.println(obj2.x());
+		
+	}
+
+}
